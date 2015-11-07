1e939e9c6457acc11ccdf6f1892de8df160fcb18
Georgi Tsaklev <y0c0vc@gmail.com>
2015-11-07 17:33:56 +0000

    Added create inspection endpoint

--- apiary/api/controllers/InspectionController.js
+++ apiary/api/controllers/InspectionController.js
@@ -1,5 +1,38 @@
 module.exports = {
   create: function(req, res) {
+    return Hive.findOne(req.param("hive"))
+    .then(function(hive) {
+      return {
+        hive: hive.id,
+        givenHive: hive.givenId,
+        date: new Date(req.param("date")),
+        weather: req.param("weather"),
+        state: req.param("state"),
+        temper: req.param("temper"),
+        queens: req.param("queens"),
 
+        combCondition: {
+          honey: req.param("honey"),
+          pollen: req.param("pollen")
+        },
+
+        diseases: {
+          beetles: req.param("beetles"),
+          mites: req.param("mites"),
+          ants: req.param("ants"),
+          chalk: req.param("chalk")
+        },
+
+        hiveCondition: req.param("hiveCondition"),
+        tools: req.param("tools")
+      };
+    }).then(function(newInspection) {
+      return Inspection.create(newInspection);
+    }).catch(function(err) {
+      sails.log.error("Failed to create new Inspection", err);
+      return {
+        error: "Failed to create new Inspection"
+      };
+    }).then(res.send);
   }
 };
--- apiary/api/models/Inspection.js
+++ apiary/api/models/Inspection.js
@@ -1,42 +1,44 @@
 module.exports = {
-  hive: {
-    model: "hive"
-  },
-  givenHive: {
-    type: "string"
-  },
-  date: {
-    type: "date",
-    defaultsTo: function() {
-      return new Date();
+  attributes: {
+    hive: {
+      model: "hive"
+    },
+    givenHive: {
+      type: "string"
+    },
+    date: {
+      type: "date",
+      defaultsTo: function() {
+        return new Date();
+      }
+    },
+    weather: {
+      type: "string"
+    },
+    state: {
+      type: "string"
+    },
+    temper: {
+      type: "string"
+    },
+    queens: {
+      type: "string"
+    },
+
+    combCondition: {
+      type: "json"
+    },
+
+    diseases: {
+      type: "json"
+    },
+
+    hiveCondition: {
+      type: "string"
+    },
+
+    tools: {
+      type: "string"
     }
-  },
-  weather: {
-    type: "string"
-  },
-  state: {
-    type: "string"
-  },
-  temper: {
-    type: "string"
-  },
-  queens: {
-    type: "string"
-  },
-
-  combCondition: {
-    type: "json"
-  },
-
-  diseases: {
-    type: "json"
-  },
-
-  hiveCondition: {
-    type: "string"
-  },
-
-  tools: {
-    type: "string"
   }
 };
