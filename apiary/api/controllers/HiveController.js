54b0e8fda2d046656de36dd825c9ba05dcb74b26
Georgi Tsaklev <y0c0vc@gmail.com>
2015-11-07 17:17:24 +0000

    Added create hive endpoint

--- apiary/api/controllers/HiveController.js
+++ apiary/api/controllers/HiveController.js
@@ -1,5 +1,31 @@
 module.exports = {
   create: function(req, res) {
-    
+    var lat = parseFloat(req.param("lat"));
+    var log = parseFloat(req.param("log"));
+
+    if (isNaN(lat) || isNaN(log)) {
+      return res.send({
+        error: "incorrect gps data received"
+      });
+    }
+
+    var newHive = {
+      givenId: req.param("givenId"),
+
+      user: req.user.id,
+
+      // GPS
+      lat: lat,
+      log: log,
+
+      installationDate: new Date(req.param("installDate")),
+      type: req.param("type"),
+      exposure: req.param("exposure")
+    };
+
+    Hive.create(newHive)
+    .then(function(hive) {
+      return res.send(hive);
+    });
   }
 };
