543035e63c6bfd176976bad5725aac230a47780f
Georgi Tsaklev <y0c0vc@gmail.com>
2015-11-07 17:04:31 +0000

    Implementation of create apiary endpoint

--- apiary/api/controllers/ApiaryController.js
+++ apiary/api/controllers/ApiaryController.js
@@ -1,5 +1,52 @@
 module.exports = {
   create: function(req, res) {
-    
+    var lat = parseFloat(req.param("lat"));
+    var log = parseFloat(req.param("log"));
+
+    if (isNaN(lat) || isNaN(log)) {
+      return res.send({
+        error: "incorrect gps data received"
+      });
+    }
+
+    var newApiary = {
+      name: req.param("name"),
+
+      user: req.user.id,
+
+      // GPS
+      lat: lat,
+      log: log,
+
+      startYear: new Date(req.param("date")),
+
+      // questions
+      environment: {
+        water: req.param("water"),
+        vegetationMiombo: req.param("miombo"),
+        vegetationForests: req.param("forests"),
+        vegetationGrass: req.param("grass"),
+        vegetationForestPlantation: req.param("forestPlantation"),
+        vegetationSisalPlantation: req.param("sisalPlantation"),
+        vegetationOrchard: req.param("orchard"),
+        vegetationMixed: req.param("mixed"),
+        pesticides: req.param("pesticides")
+      },
+      accessibility: {
+        vehicle: req.param("vehicle"),
+        cycle: req.param("cycle"),
+        foot: req.param("foot"),
+        natural: req.param("natural"),
+        tree: req.param("tree"),
+        breastHeight: req.param("height"),
+        beeHouse: req.param("beeHouse"),
+        badger: req.param("badger")
+      }
+    };
+
+    Apiary.create(newApiary)
+    .then(function(apiary){
+      res.send(apiary);
+    });
   }
 };
