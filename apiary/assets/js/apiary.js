ee2654264d8bdca2fab35afe6ceda6e3ce8b4101
Stefan <sk@inocr8.co>
2015-11-07 15:48:41 +0000

    Added angular strap. Added navbar buttons. Create apiary routing page

--- apiary/assets/js/apiary.js
+++ apiary/assets/js/apiary.js
@@ -1,6 +1,7 @@
 var app = angular.module('apiary', [
     'ui.router',
-    'mainModule'
+    'mainModule',
+    'mgcrea.ngStrap'
 ]);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
@@ -12,15 +13,27 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 
     $stateProvider
         .state("main", {
-            url: "",
-            views: {
-                '': {
-                    templateUrl: 'templates/layout.html',
-                    controller: 'mainController'
-                },
-                'topbar@main': {
-                    templateUrl: '/templates/navbar.html',
-                }
-              }
-            })
+          url: "",
+          views: {
+            '': {
+              templateUrl: 'templates/layout.html',
+              controller: 'mainController'
+            },
+            'topbar@main': {
+              templateUrl: '/templates/navbar.html',
+            },
+            'body@main': {
+              templateUrl: '/templates/home.html'
+            }
+          }
+        })
+        .state("main.apiary", {
+          url: "/apiary",
+          views: {
+            'body@main': {
+              templateUrl: '/templates/apiary.html',
+              controller: 'apiaryController'
+            }
+          }
+        })
 }])
--- apiary/assets/js/main.js
+++ apiary/assets/js/main.js
@@ -7,3 +7,12 @@ app.controller("mainController", [function() {
 app.controller("masterController", [function() {
 
 }])
+
+app.controller("apiaryController", ["$scope", function($scope) {
+  $scope.apiry = {}
+
+  $scope.create = function() {
+    
+  }
+
+}])
