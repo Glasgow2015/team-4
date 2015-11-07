9ddba70afbb4bae78e6e27791099d2dff76f7a36
ryancook <lilryancook@hotmail.co.uk>
2015-11-07 19:04:10 +0000

    New Apiary button added, taking user to create new apiary activity

---
+++ android-app/app/src/main/java/com/jpmorganchase/apiary/ApiaryActivity.java
@@ -0,0 +1,9 @@
+package com.jpmorganchase.apiary;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by RyanCook on 07/11/2015.
+ */
+public class ApiaryActivity extends AppCompatActivity {
+}
