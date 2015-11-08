073a2a2840c9f1364e3f077816ab361593f23f8b
Cristian Szabo <cristian.szabo@videogamelab.co.uk>
2015-11-08 04:00:12 +0000

    Add login system

--- android-app/app/src/main/java/com/jpmorganchase/apiary/LoginActivity.java
+++ android-app/app/src/main/java/com/jpmorganchase/apiary/LoginActivity.java
@@ -3,20 +3,25 @@ package com.jpmorganchase.apiary;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Authenticator;
 import java.net.HttpURLConnection;
 import java.net.PasswordAuthentication;
 import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
-    static String host = "http://www.google.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,15 +54,38 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         protected Long doInBackground(Object... x) {
-            HttpURLConnection urlConnection;
             try {
-                URL conn = new URL(host);
-                urlConnection = (HttpURLConnection) conn.openConnection();
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                System.out.println(urlConnection.getResponseMessage());
+                String url="http://ec2-54-216-204-98.eu-west-1.compute.amazonaws.com:8080/api/auth/local/register";
+                URL object=new URL(url);
 
-                if (!conn.getHost().equals(urlConnection.getURL().getHost())) {
+                HttpURLConnection con = (HttpURLConnection) object.openConnection();
+                con.setDoOutput(true);
+                con.setDoInput(true);
+                con.setRequestProperty("Content-Type", "application/json");
+                con.setRequestProperty("Accept", "application/json");
+                String userpass = username + ":" + password;
+                String basicAuth = "Basic " + new String(Base64.encode(userpass.getBytes("UTF-8"), Base64.DEFAULT));
+                con.setRequestProperty ("Authorization", basicAuth);
+                con.setRequestMethod("POST");
 
+                OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+                wr.flush();
+
+                StringBuilder sb = new StringBuilder();
+                int HttpResult = con.getResponseCode();
+                if(HttpResult == HttpURLConnection.HTTP_OK){
+                    BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(),"utf-8"));
+                    String line = null;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+
+                    br.close();
+
+                    System.out.println(""+sb.toString());
+
+                }else{
+                    Log.e("Bees", con.getResponseMessage());
                 }
             } catch (Exception e) {
                 Log.e("Bees", e.getMessage());
