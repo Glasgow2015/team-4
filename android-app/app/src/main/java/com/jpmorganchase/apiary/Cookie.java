e16e798123a19c9de6c5a622e6e70543afa97f6d
Reni <aqua1111111@gmail.com>
2015-11-08 05:12:34 +0000

    added Cookie class w/ static cookie

---
+++ android-app/app/src/main/java/com/jpmorganchase/apiary/Cookie.java
@@ -0,0 +1,10 @@
+package com.jpmorganchase.apiary;
+
+import java.net.CookieManager;
+
+/**
+ * Created by reni on 08/11/15.
+ */
+public class Cookie {
+    static CookieManager manager = new CookieManager();
+}
